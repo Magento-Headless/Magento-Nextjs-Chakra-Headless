155c9171943bf5d12400b4fe9c50f1729e4e339b
caesar <454451758@qq.com>
2022-10-29 07:57:44 +0800

    chore: chakra ui

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.REACT_APP_BUNDLE_VISUALIZE === '1'
+})
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = (phase, { defaultConfig }) => {
+  //** @type {import('next').NextConfig} */
+  const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+  }
+
+  return isProd ? withBundleAnalyzer(nextConfig) : nextConfig
+}
